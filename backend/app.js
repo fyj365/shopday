d566a863e55177a9ee95bb89dc73045b88c64c2e
fiona <fangyj111@gmail.com>
2021-11-23 00:54:40 +0800

    fix deployment 2

--- backend/app.js
+++ backend/app.js
@@ -18,17 +18,22 @@ const products = require('./routes/product');
 const users = require('./routes/auth');
 const orders =require('./routes/order');
 
+
+
+app.get('/*', (req, res) => {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+})
+
 app.use('/api/v1', products);
 app.use('/api/v1', users);
 app.use('/api/v1', orders);
-
-
-if(process.env.NODE_ENV == 'PRODUCTION' ) {
-    app.get('*', (req, res) => {
-        app.use(express.static(path.join(__dirname, '../frontend/build')))
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-    })
-}
+// if(process.env.NODE_ENV == 'PRODUCTION' ) {
+//     app.get('*', (req, res) => {
+//         app.use(express.static(path.join(__dirname, '../frontend/build')))
+//         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+//     })
+// }
 
 // Middleware to handle errors
 app.use(errorMiddleware)
