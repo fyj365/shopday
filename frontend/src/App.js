e580f79c04b7f151263a8366c504f10e44595af5
fiona <fangyj111@gmail.com>
2021-11-23 01:54:17 +0800

    fix heroku deployment 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
-import './App.css';
+import './App.css'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
