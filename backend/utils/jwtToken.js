9215b2e0743d326d6ad575ab8c8ba318d9021f56
fiona <fangyj111@gmail.com>
2021-11-23 15:32:44 +0800

    fix env variables

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,7 +5,7 @@ const sendToken = (user, statusCode, res) => {
     // options for cookie
     const options = {
         expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000
+            Date.now() + parseInt(`${process.env.COOKIE_EXPIRE_TIME}`) * 24 * 60 * 60 * 1000
         ),
         httpOnly: true
     }
